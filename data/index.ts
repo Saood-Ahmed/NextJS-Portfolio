b702fcd01b0cecee5917df804edb2a2fe0af88a7
Saood-Ahmed <unknownsuperhero@yahoo.com>
2024-08-07 18:51:45 +0500

    Updated files

---
+++ data/index.ts
@@ -0,0 +1,245 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently polishing my skills as a Full Stack Developer",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: " GenAI-Course-Tasks Using TypeScript",
+      des: "TypeScript First Assignment Submission (All 45 Questions) by Syed Saood Ahmed",
+      img: "/tech1.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/js.svg"],
+      link: "/github.com/Saood-Ahmed/GenAI-Course-Tasks",
+    },
+    {
+      id: 2,
+      title: "Grocery App Using React",
+      des: "Whether you're planning your weekly grocery haul or need a few items for a special recipe, our app has got you covered.",
+      img: "/cart-app.png",
+      iconLists: ["/re.svg", "/bootstrap.svg", "/ts.svg"],
+      link: "/github.com/Saood-Ahmed/cart-app",
+    },
+    {
+      id: 3,
+      title: "Smart Budget Tracker: Manage Your Finances with Ease",
+      des: "Take charge of your finances today with Smart Budget Tracker. Download now and start managing your budget like a pro with the power of React and the reliability of GitHub Actions.",
+      img: "/grocery.avif",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/bootstrap.svg"],
+      link: "/my-exp-tracker-app.surge.sh/",
+    },
+    {
+      id: 4,
+      title: "Quiz App",
+      des: "Whether you’re a trivia enthusiast, a student, or just looking to challenge your knowledge, Our Quiz App has something for everyone.",
+      img: "/quiz.avif",
+      iconLists: ["/re.svg", "/bootstrap.svg", "/ts.svg"],
+      link: "/myquiz-app.surge.sh/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Saood has been a pleasure to work with throughout his time at Blink. He is very intelligent and brings a solid work ethic to our design team. As a designer, he has grown tremendously and continues to challenge himself daily. Thank you for all your hard work Saood!",
+      name: "Carol Cardova",
+      title: "Senior Graphic Designer (Blink Ventures)",
+    },
+    {
+      quote:
+        "I'm pleased to recommend Saood for his outstanding expertise in web development, particularly in MERN Stack and PHP. I had the pleasure of working closely with Saood on several projects where he consistently delivered exceptional results. Saood possesses a keen eye for detail, ensuring that projects are not only completed on time but also exceed expectations. His collaborative nature and willingness to cooperate make him an invaluable asset to any team. I highly recommend Saood for any web development endeavors and am confident he will excel in any future endeavors.",
+      name: "Hashir Haider Khattak",
+      title: "Team Lead Software Engineer at Matz Solutions Pvt Ltd",
+    },
+    {
+      quote:
+        "Worked with Saood at his time at DevelopEirs (now Syslify) and found him to be extremely professional, hardworking and especially helpful. I was quite new to the industry and he really helped me and guided me through many difficulties.",
+      name: "Hadi Khan",
+      title: "Backend & DevOps - I automate automation. || NUST'26 || CF @ Parhlai.com",
+    },
+    {
+      quote:
+        "As a project manager, I had the pleasure of collaborating with Saood, who served as our designer. Saood's keen eye for detail and creative prowess greatly enhanced our project outcomes. His proactive communication and ability to swiftly understand project requirements made him an invaluable asset to our team. I confidently recommend Saood for any design project, knowing that his dedication and talent will consistently exceed expectations.",
+      name: "Themoor Shaikh",
+      title: "Project Manager at Blink Signs",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "react",
+      img: "/re.svg",
+      nameImg: "/reName.svg",
+    },
+    {
+      id: 2,
+      name: "typescript",
+      img: "/ts.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "tailwind",
+      img: "/tail.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "NextJs",
+      img: "/next.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+    {
+      id: 6,
+      name: "nodejs.",
+      img: "/nodejs.png",
+      nameImg: "/dockerName.svg",
+    },
+    {
+      id: 7,
+      name: "figma",
+      img: "/figma.svg",
+      nameImg: "/dockerName.svg",
+    },
+    {
+      id: 8,
+      name: "MySql",
+      img: "/mysql-icon.svg",
+      nameImg: "/dockerName.svg",
+    },
+    {
+      id: 9,
+      name: "MongoDB",
+      img: "/mongodb.svg",
+      nameImg: "/dockerName.svg",
+    },
+    {
+      id: 10,
+      name: "Express",
+      img: "/express.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Full Stack Developer - MATZ Solutions Pvt Ltd.",
+      desc: "Working as a Full Stack Developer proficient in MERN and PHP/MySQL, with a touch of UI/UX expertise. Developed full-scale applications, including real estate projects and inventory systems.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Associate Software Engineer - XGEN Technologies",
+      desc: "With expertise in the MERN stack and PHP/MySQL development, delivering robust web solutions. Specializing in front-end development while contributing to backend tasks, ensuring secure and efficient server-side logic.",
+      className: "md:col-span-2",
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Full Stack Developer & UI Designer - Syslify",
+      desc: "As a Full Stack Developer and UI Designer for CryptoKit, I utilized React for front-end development and created APIs using Node.js. Additionally, I worked on a complete product related to crypto and NFT, with experience in Docker and Jenkins.",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Graphic Designer - Blink Ventures",
+      desc: "Designed signage for leading USA-based clients like Verizon, Choice Hotels, and Amazon. Created proposals for both interior and exterior displays, and occasionally delved into product design and mockups.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+];
+
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/saood-ahmed",
+    },
+    {
+      id: 2,
+      img: "/behance.svg",
+      link: "https://www.behance.net/saood1"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/saood",
+    },
+  ];
